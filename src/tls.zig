2036b75d82074f21590133a4248fac6f0bb4fca1
ddddddO <lbfdeatq@gmail.com>
2022-08-03 15:50:29 +0900

    modify

--- src/tls.zig
+++ src/tls.zig
@@ -101,6 +101,16 @@ const CipherSuite = enum {
     }
 };
 
+const CompressionMethod = enum {
+    Null,
+
+    fn bytes(self: CompressionMethod) [1]u8 {
+        return switch (self) {
+            CompressionMethod.Null => [1]u8{'\x00'},
+        };
+    }
+};
+
 const TLSRecordLayer = struct {
     allocator: Allocator,
 
@@ -115,21 +125,26 @@ const TLSRecordLayer = struct {
         // ref: https://realizeznsg.hatenablog.com/entry/2018/09/17/110000
         // 以下は、wiresharkを起動し、curl https://app-dot-tag-mng-243823.appspot.com を実行し、「Client Hello」のパケットを調べて少し手を加えた。
         // 実行するとwiresharkで「[Client Hello Fragment], Ignore Unkown Record」と確認できた。
+        //   -> https://milestone-of-se.nesuke.com/nw-basic/tls/https-structure/
+        //   -> 変更後、[Client Hello] の表示を確認。[Client Hello]の後に、サーバーからACK -> [Continuation Data]が送られてきていた。
+
+        // wiresharkで、対象のパケットを選択して、「コピー」 -> 「as a Hex Stream」後、以下
+        // echo a01b23456789 | sed -E ":l; s/^([0-9a-z]+)([a-z0-9]{2})/\1,\'\\\x\2\'/; t l;";
         var client_hello_buf = std.ArrayList(u8).init(self.allocator);
 
         const content_type = ContentType.handshake.bytes();
-        const version = TLSVersion.v1_2.bytes();
-        const content_length = [_]u8{ '\x00', '\x2a' }; // 42
+        const version_a = TLSVersion.v1_0.bytes();
+        const content_length = [_]u8{ '\x01', '\x62' }; // 4 + 43 + 307(sum extensions) = 354
         try client_hello_buf.appendSlice(&content_type);
-        try client_hello_buf.appendSlice(&version);
+        try client_hello_buf.appendSlice(&version_a);
         try client_hello_buf.appendSlice(&content_length);
 
         const handshake_type = HandshakeType.client_hello.bytes();
-        // sum 46 bytes?
-        const length = [_]u8{ '\x00', '\x00', '\x2e' }; // 3
+        const length = [_]u8{ '\x00', '\x01', '\x5e' }; // 3: 354 - 4 = 350
+        const version_b = TLSVersion.v1_2.bytes();
         try client_hello_buf.appendSlice(&handshake_type);
         try client_hello_buf.appendSlice(&length);
-        try client_hello_buf.appendSlice(&version);
+        try client_hello_buf.appendSlice(&version_b);
 
         // random
         const unix_time = [_]u8{ '\x2f', '\xe5', '\xd6', '\xfe' }; // 4
@@ -146,12 +161,28 @@ const TLSRecordLayer = struct {
         try client_hello_buf.appendSlice(&cipher_suite);
 
         const compression_methods_length = [_]u8{'\x01'}; // 1
-        const compression_method = [_]u8{'\x00'}; // 1
+        const compression_method = CompressionMethod.Null.bytes(); // 1
         try client_hello_buf.appendSlice(&compression_methods_length);
         try client_hello_buf.appendSlice(&compression_method);
 
-        const extensions_length = [_]u8{ '\x00', '\x00' }; // 2
+        const extensions_length = [_]u8{ '\x01', '\x33' }; // =307 =sum extensions
         try client_hello_buf.appendSlice(&extensions_length);
+        const extension_server_name = [_]u8{ '\x00', '\x00', '\x00', '\x27', '\x00', '\x25', '\x00', '\x00', '\x22', '\x61', '\x70', '\x70', '\x2d', '\x64', '\x6f', '\x74', '\x2d', '\x74', '\x61', '\x67', '\x2d', '\x6d', '\x6e', '\x67', '\x2d', '\x32', '\x34', '\x33', '\x38', '\x32', '\x33', '\x2e', '\x61', '\x70', '\x70', '\x73', '\x70', '\x6f', '\x74', '\x2e', '\x63', '\x6f', '\x6d' }; // 39
+        try client_hello_buf.appendSlice(&extension_server_name);
+        const extension_ec_point_formats = [_]u8{ '\x00', '\x0b', '\x00', '\x04', '\x03', '\x00', '\x01', '\x02' }; // 4
+        try client_hello_buf.appendSlice(&extension_ec_point_formats);
+        const extension_supported_groups = [_]u8{ '\x00', '\x0a', '\x00', '\x1c', '\x00', '\x1a', '\x00', '\x17', '\x00', '\x19', '\x00', '\x1c', '\x00', '\x1b', '\x00', '\x18', '\x00', '\x1a', '\x00', '\x16', '\x00', '\x0e', '\x00', '\x0d', '\x00', '\x0b', '\x00', '\x0c', '\x00', '\x09', '\x00', '\x0a' }; // 28
+        try client_hello_buf.appendSlice(&extension_supported_groups);
+        const extension_signature_algorithms = [_]u8{ '\x00', '\x0d', '\x00', '\x20', '\x00', '\x1e', '\x06', '\x01', '\x06', '\x02', '\x06', '\x03', '\x05', '\x01', '\x05', '\x02', '\x05', '\x03', '\x04', '\x01', '\x04', '\x02', '\x04', '\x03', '\x03', '\x01', '\x03', '\x02', '\x03', '\x03', '\x02', '\x01', '\x02', '\x02', '\x02', '\x03' }; // 32
+        try client_hello_buf.appendSlice(&extension_signature_algorithms);
+        const extension_heartbeat = [_]u8{ '\x00', '\x0f', '\x00', '\x01', '\x01' };
+        try client_hello_buf.appendSlice(&extension_heartbeat);
+        const extension_next_protocol_negotiation = [_]u8{ '\x33', '\x74', '\x00', '\x00' };
+        try client_hello_buf.appendSlice(&extension_next_protocol_negotiation);
+        const extension_application_layer_protocol_negotiation = [_]u8{ '\x00', '\x10', '\x00', '\x0b', '\x00', '\x09', '\x08', '\x68', '\x74', '\x74', '\x70', '\x2f', '\x31', '\x2e', '\x31' };
+        try client_hello_buf.appendSlice(&extension_application_layer_protocol_negotiation);
+        const extension_padding = [_]u8{ '\x00', '\x15', '\x00', '\xa0', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00' };
+        try client_hello_buf.appendSlice(&extension_padding);
 
         return client_hello_buf.items;
     }
